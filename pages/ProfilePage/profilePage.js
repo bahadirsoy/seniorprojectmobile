b25b97d6f8131b0a30dc0346ec6a441c28bb4afc
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-26 00:46:38 +0300

    user informations are fetched in profile page

--- pages/ProfilePage/profilePage.js
+++ pages/ProfilePage/profilePage.js
@@ -1,7 +1,27 @@
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import styles from './profilePage.styles.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProfilePage = () => {
+
+    //userId
+    const [userInfo, setUserInfo] = useState()
+
+    useEffect(() => {
+        AsyncStorage.getItem("userId").then(userId => {
+            axios.get("https://bezkoder-server.herokuapp.com/api/getUserInformationsWithUserId", {
+                params: {
+                    userId: userId
+                }
+            })
+            .then(response => {
+                setUserInfo(response.data[0])
+            })
+        })
+    }, [])
+
     return(
         <View>
             <Text>brom</Text>
