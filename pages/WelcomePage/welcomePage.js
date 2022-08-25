0d4cbd49ab2417d78c5784b742babba1ba98b29b
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-25 15:08:06 +0300

    welcome page content is added

--- pages/WelcomePage/welcomePage.js
+++ pages/WelcomePage/welcomePage.js
@@ -3,7 +3,26 @@ import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'reac
 const WelcomePage = () => {
     return(
         <View>
-            <Text>welcome page</Text>
+            <View>
+                <Text>Welcome</Text>
+            </View>
+
+            <View>
+                <Text>
+                    CheckRoommate is social media like program that helps you to find roommate. 
+                    You must sign in to continue. You can create new account if you don't have one yet.
+                </Text>
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Text>Log in</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text>Sign up</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
