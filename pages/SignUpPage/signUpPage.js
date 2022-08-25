490f9416d1fc2a5bfb3a03d3cbb343e23cf3ba7d
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-25 14:37:17 +0300

    user can sign up

--- pages/SignUpPage/signUpPage.js
+++ pages/SignUpPage/signUpPage.js
@@ -3,8 +3,35 @@ import styles from './signUpPage.styles.js'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { Formik } from 'formik';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
-const SignUpPage = () => {
+const SignUpPage = (props) => {
+
+    const signUp = (values) => {
+        const {username, password, name, surname, email} = values
+
+        const trimmedUsername = username.trim()
+        const trimmedName = name.trim()
+        const trimmedSurname = surname.trim()
+        const trimmedEmail = email.trim()
+
+        axios.post('https://bezkoder-server.herokuapp.com/api/insertUser', {
+            username: trimmedUsername, 
+            password: password,
+            name: trimmedName,
+            surname: trimmedSurname,
+            email: trimmedEmail
+        }).then(response => {
+            if(response.data.error){
+                if(response.data.error.errno == 1062){
+                    console.log("Username taken brom")
+                }
+            } else{
+                console.log("signed up brom")
+            }
+        })
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.signUpIcon}>
@@ -19,9 +46,12 @@ const SignUpPage = () => {
                     surname: "",
                     email: ""
                 }}
+                onSubmit={(values) => {
+                    signUp(values)
+                }}
             >
 
-            {({handleChange, values}) => {
+            {({handleChange, values, handleSubmit}) => {
                 const {username, password, name, surname, email} = values
 
                 return(
@@ -72,7 +102,7 @@ const SignUpPage = () => {
                         </View>
 
                         <View style={styles.signUpButtonView}>
-                            <TouchableOpacity style={styles.signUpButton}>
+                            <TouchableOpacity style={styles.signUpButton} onPress={handleSubmit} >
                                 <Text style={styles.signUpButtonText}>Sign up</Text>
                             </TouchableOpacity>
                         </View>
@@ -83,7 +113,7 @@ const SignUpPage = () => {
             </Formik>
 
             <View style={styles.alert}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => {props.navigation.navigate("SignInPage")}}>
                     <Text style={styles.alertText1}>
                         Already have an account ?
                         <Text style={styles.alertText2}> Sign in</Text>
