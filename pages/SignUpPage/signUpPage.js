2c83fda17eb8bba076c2be398f34355705844407
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-25 13:45:58 +0300

    sign up page frontend is added

--- pages/SignUpPage/signUpPage.js
+++ pages/SignUpPage/signUpPage.js
@@ -1,9 +1,95 @@
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import styles from './signUpPage.styles.js'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { Formik } from 'formik';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 const SignUpPage = () => {
     return(
-        <View>
-            <Text>sign up page</Text>
+        <View style={styles.container}>
+            <View style={styles.signUpIcon}>
+                <FontAwesomeIcon icon={ faUserPlus } size={100} />
+            </View>
+
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: "",
+                    name: "",
+                    surname: "",
+                    email: ""
+                }}
+            >
+
+            {({handleChange, values}) => {
+                const {username, password, name, surname, email} = values
+
+                return(
+                    <View style={styles.form}>
+                        <View style={styles.inputItem}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder="Username"
+                                onChangeText={handleChange("username")}
+                                value={username}
+                            />
+                        </View>
+
+                        <View style={styles.inputItem}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder="Password"
+                                onChangeText={handleChange("password")}
+                                value={password}
+                            />
+                        </View>
+
+                        <View style={styles.inputItem}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder="Name"
+                                onChangeText={handleChange("name")}
+                                value={name}
+                            />
+                        </View>
+
+                        <View style={styles.inputItem}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder="Surname"
+                                onChangeText={handleChange("surname")}
+                                value={surname}
+                            />
+                        </View>
+
+                        <View style={styles.inputItem}>
+                            <TextInput
+                                style={styles.textInput}
+                                placeholder="E-mail"
+                                onChangeText={handleChange("email")}
+                                value={email}
+                            />
+                        </View>
+
+                        <View style={styles.signUpButtonView}>
+                            <TouchableOpacity style={styles.signUpButton}>
+                                <Text style={styles.signUpButtonText}>Sign up</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )
+            }}
+
+            </Formik>
+
+            <View style={styles.alert}>
+                <TouchableOpacity>
+                    <Text style={styles.alertText1}>
+                        Already have an account ?
+                        <Text style={styles.alertText2}> Sign in</Text>
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
---
+++ pages/SignUpPage/signUpPage.styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    signUpIcon: {
+        alignItems: "center"
+    },
+    form: {
+
+    },
+    inputItem: {
+        paddingVertical: 15,
+        paddingHorizontal: 25
+    },
+    textInput: {
+        borderWidth: 1,
+        padding: 5,
+    },
+    signUpButtonView:{
+        paddingVertical: 25,
+        paddingHorizontal: 75
+    },
+    signUpButton: {
+        backgroundColor: "lightblue",
+        paddingVertical: 18,
+        alignItems: "center",
+    },
+    signUpButtonText: {
+        fontSize: 20,
+        color: "black"
+    },
+    alert: {
+        alignItems: "center"
+    },
+    alertText1:{
+        fontSize: 18
+    },
+    alertText2: {
+        color: "orange"
+    }
+})
+
