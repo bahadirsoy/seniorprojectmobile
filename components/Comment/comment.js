07fa1ef2336b43fa3a7c97e808226021d989bcd2
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-26 23:06:17 +0300

    username is fetched in comment component

--- components/Comment/comment.js
+++ components/Comment/comment.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import styles from './comment.styles.js';
+import axios from 'axios';
 
 const Comment = ({commentContent, userId}) => {
+
+    //username
+    const [username, setUsername] = useState('')
+
+    useEffect(() => {
+        getUsernameFromId()
+    })
+
+    //get getUsernameFromId
+    const getUsernameFromId = (() => {
+        axios.get("https://bezkoder-server.herokuapp.com/api/getUsernameFromId", {
+            params: {
+                userId: userId
+            }
+        })
+        .then((response) => {
+            setUsername(response.data[0].username)
+        })
+    })
+
     return(
-        <View>
+        <View style={styles.container}>
             <TouchableOpacity>
-                <Text>Username</Text>
+                <Text> {username} </Text>
             </TouchableOpacity>
 
             <Text> {commentContent} </Text>
