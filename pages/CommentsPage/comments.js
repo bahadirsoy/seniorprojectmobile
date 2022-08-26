b54df52df13f9ada2f7694e14d937018570f45e2
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-08-26 22:55:37 +0300

    comments are fetched in comments page

--- pages/CommentsPage/comments.js
+++ pages/CommentsPage/comments.js
@@ -1,20 +1,47 @@
-import { useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import styles from './comments.styles.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const CommentsPage = (props) => {
 
-    useEffect(() => {
-        AsyncStorage.getItem("userId").then(userId => {
+    //userId
+    const [userId, setUserId] = useState()
 
+    //comments
+    const [comments, setComments] = useState()
+
+    useEffect(() => {
+        //get userId
+        AsyncStorage.getItem("userId").then(userId => {
+            setUserId(userId)
         })
-    })
+
+        getComments()
+    }, [])
+
+    //get comments
+    const getComments = () => {
+        axios.get("https://bezkoder-server.herokuapp.com/api/getPostComments", {
+            params: {
+                postId: props.route.params.postId
+            }
+        })
+        .then((response) => {
+            setComments(response.data)
+        })
+    }
 
     return(
         <View>
-            <Text>devamke</Text>
+            <FlatList
+                data={comments}
+                renderItem={({item}) => 
+                    <Text>kank</Text>
+                }
+                keyExtractor={(item) => item.postCommentId}
+            />
         </View>
     )
 }
