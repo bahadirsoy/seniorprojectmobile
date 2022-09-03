a4acaa272aa43f4aa62ada4021cd1706bceb40ce
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-03 03:45:24 +0300

    user informations page shows real data and user reviews

--- pages/UserInformationsPage/userInformations.js
+++ pages/UserInformationsPage/userInformations.js
@@ -1,17 +1,78 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import styles from './userInformations.styles.js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { ActivityIndicator } from '@react-native-material/core';
+import Comment from '../../components/Comment/comment.js';
 
-const UserInformations = () => {
+
+const UserInformations = (props) => {
+
+    //user informations
+    const [userInformations, setUserInformations] = useState()
+
+    //user reviews
+    const [userReviews, setUserReviews] = useState()
+
+    useEffect(() => {
+        getUserInformations()
+        getUserReviews()
+    }, [])
+
+    //get all user profile informations and set state
+    const getUserInformations = () => {
+        axios.get("https://bezkoder-server.herokuapp.com/api/getUserInformationsWithUserId", {
+            params: {
+                userId: props.route.params.userId
+            }
+        })
+        .then((response) => {
+            setUserInformations({
+                userId: response.data[0].userId,
+                username: response.data[0].username,
+                name: response.data[0].name,
+                surname: response.data[0].surname,
+                email: response.data[0].email,
+                phone: response.data[0].phone,
+            })
+        })
+    }
+
+    //get all user reviews
+    const getUserReviews = () => {
+        axios.get("https://bezkoder-server.herokuapp.com/api/getUserReviews", {
+            params: {
+                reviewedId: props.route.params.userId
+            }
+        })
+        .then((response) => {
+            setUserReviews(response.data)
+        })
+    }
+
     return(
         <View style={styles.container}>
-            <View style={styles.infoContainer}>
-                <Text style={styles.infoText}> Username: </Text>
-                <Text style={styles.infoText}> Name: </Text>
-                <Text style={styles.infoText}> Surname: </Text>
-                <Text style={styles.infoText}> E-mail: </Text>
-                <Text style={styles.infoText}> Phone: </Text>
+            <View style={styles.infoContainer}>{console.log(userReviews)}
+                <Text style={styles.infoText}> Username: {userInformations ? userInformations.username : <ActivityIndicator size="small" color="error" />} </Text>
+                <Text style={styles.infoText}> Name: {userInformations ? userInformations.name : <ActivityIndicator size="small" color="error" />} </Text>
+                <Text style={styles.infoText}> Surname: {userInformations ? userInformations.surname : <ActivityIndicator size="small" color="error" />} </Text>
+                <Text style={styles.infoText}> E-mail: {userInformations ? userInformations.email : <ActivityIndicator size="small" color="error" />} </Text>
+                <Text style={styles.infoText}> Phone: {userInformations ? userInformations.phone : <ActivityIndicator size="small" color="error" />} </Text>
             </View>
+
+            <Text style={{fontSize: 24, fontWeight: "600", alignSelf: "center"}}>User Reviews</Text>
+            <FlatList
+                data={userReviews}
+                renderItem={({item}) => 
+                    <Comment
+                        userId={item.reviewerId}
+                        commentContent={item.reviewContent}
+                    />
+                }
+                keyExtractor={(item) => item.userreviewId}
+                style={{marginBottom: 100}}
+            />
         </View>
     )
 }
--- pages/UserInformationsPage/userInformations.styles.js
+++ pages/UserInformationsPage/userInformations.styles.js
@@ -3,11 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "pink"
     },
     infoContainer: {
         flex:1,
-        marginTop: "30%"
+        marginTop: "10%"
     },
     infoText:{
         fontSize: 18,
